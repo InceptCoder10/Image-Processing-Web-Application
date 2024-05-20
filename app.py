d269b848d9a0955fbb357b1abeee1764d241f554
Gaurav Yadav <103851035+InceptCoder10@users.noreply.github.com>
2024-05-20 12:26:03 +0530

    Fixed and Updated Background Remove feature

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, send_file
+from rembg import remove
 from PIL import Image, ImageEnhance
 import io
 
@@ -16,12 +17,16 @@ def adjust_saturation(image, factor):
     enhancer = ImageEnhance.Color(image)
     return enhancer.enhance(factor)
 
+def remove_bg(image):
+    output=remove(image)
+    return output 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/remove', methods=['POST'])
-def remove():
+def remove_background():
     if 'file' not in request.files:
         return "No file part"
 
@@ -29,12 +34,16 @@ def remove():
     if file.filename == '':
         return "No selected file"
     
-    img= Image.open(file)
+    try:
+        img = Image.open(file)
+    except IOError:
+        return "File is not a valid image",400
+    
+    output_remove = remove_bg(img)
     output = io.BytesIO()
-    output = remove(img)
+    output_remove.save(output,format='png')
     output.seek(0)
-
-    return send_file(output, mimetype='image/jpeg')
+    return send_file(output, mimetype='image/png')
 
 
 
